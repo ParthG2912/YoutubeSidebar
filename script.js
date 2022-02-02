639b8376ea76dc432ea8ccb4355c0843a751218b
Parth Gandhi <54275328+ParthG2912@users.noreply.github.com>
2022-02-02 23:36:39 +0530

    Active Menu Done

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ menuIconBtn.addEventListener("click", () => {
     // Expand / Collapse Sidebar
     sidebar.classList.toggle("open");
 });
+
+const listItem = document.querySelectorAll("[data-list-item]");
+
+listItem.forEach((item) => {
+    item.addEventListener("click", (e) => {
+        // console.log(e.target);
+        listItem.forEach((i) => {
+            // console.log(i.textContent.trim(), i.contains(e.target));
+            i.classList.toggle("active", i.contains(e.target));
+        });
+    });
+});
